43f1a92445644f15a80edaa423c81d735076e52c
ng-ad <adolphengoga@gmail.com>
2025-02-03 19:29:10 +0200

    creating light theme

---
+++ lib/core/theme/light_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    background: Color.fromARGB(183, 96, 125, 139),
+    primary: Colors.grey.shade400,
+    secondary: Colors.grey.shade200,
+  ),
+);
