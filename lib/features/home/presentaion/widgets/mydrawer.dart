f2b6391d44e995b5e75e0b9cf2193b6b557f6ab0
ad-ng <adolphengoga@gmail.com>
2025-02-06 13:47:23 +0200

    adding drawer

--- lib/features/home/presentaion/widgets/mydrawer.dart
+++ lib/features/home/presentaion/widgets/mydrawer.dart
@@ -5,6 +5,71 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Drawer(
+      width: screenWidth * 0.6,
+      child: ListView(
+        children: [
+          // DrawerHeader(
+          //   child: ClipRRect(
+          //     borderRadius: BorderRadius.circular(20),
+          //     child: Image.network(
+          //       'https://d1covwzj3ggua9.cloudfront.net/user/Adolphe.jpg',
+          //       height: 10,
+          //       width: 10,
+          //     ),
+          //   ),
+          // ),
+          Padding(
+            padding: const EdgeInsets.all(28),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(190),
+              child: Image.network(
+                'https://d1covwzj3ggua9.cloudfront.net/user/Adolphe.jpg',
+                height: screenHeight * 0.224,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings,
+                color: Theme.of(context).colorScheme.secondary),
+            title: Text(
+              'settings',
+              style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.sell,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            title: Text('sell property',
+                style:
+                    TextStyle(color: Theme.of(context).colorScheme.secondary)),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.contact_phone_sharp,
+                color: Theme.of(context).colorScheme.secondary),
+            title: Text('contact us',
+                style:
+                    TextStyle(color: Theme.of(context).colorScheme.secondary)),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.logout,
+                color: Theme.of(context).colorScheme.secondary),
+            title: Text('log out',
+                style:
+                    TextStyle(color: Theme.of(context).colorScheme.secondary)),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, 'loginPage');
+            },
+          )
+        ],
+      ),
+    );
   }
 }
