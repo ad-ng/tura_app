873c55dda94f64ed4ec0994feb0d3c1be9651372
ad-ng <adolphengoga@gmail.com>
2025-02-09 07:26:39 +0200

    rendering html description

--- lib/features/home/presentaion/pages/singleproperty.dart
+++ lib/features/home/presentaion/pages/singleproperty.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:tura_app/features/home/data/datasources/remote/fetchAllProperties.dart';
 import 'package:tura_app/features/home/data/models/properties_model.dart';
 
@@ -37,76 +38,119 @@ class Singleproperty extends StatelessWidget {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else {
             return Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: ListView(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25, top: 20),
-                    child: Text(
-                      snapshot.data!.title!,
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.secondary,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 2, horizontal: 35),
-                    child: Text(
-                      '${snapshot.data!.price} Rwf',
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.secondary,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 20),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.location_on_outlined,
-                          color: Theme.of(context).colorScheme.secondary,
-                        ),
-                        Text(
-                          snapshot.data!.place!.name!,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 25, top: 20, bottom: 20),
+                        child: Text(
+                          snapshot.data!.title!,
                           style: TextStyle(
-                              color: Theme.of(context).colorScheme.secondary),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10),
-                    height: screenHeight * 0.32,
-                    child: Image.network(snapshot.data!.imageUrls![0]),
-                  ),
-                  Container(
-                    height: screenWidth * 0.2,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: snapshot.data!.imageUrls!.length,
-                      itemBuilder: (context, index) {
-                        // return Image.asset(snapshot.data!.imageUrls![index]);
-                        return Padding(
-                          padding: const EdgeInsets.only(left: 10, top: 5),
-                          child: Image.network(
-                            snapshot.data!.imageUrls![index],
+                            color: Theme.of(context).colorScheme.secondary,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
                           ),
-                        );
-                      },
-                    ),
-                  ),
-                  Container(
-                    child: Text(
-                      'Description',
-                      style: TextStyle(
-                          color: Theme.of(context).colorScheme.secondary),
-                    ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        height: screenHeight * 0.32,
+                        child: Image.network(snapshot.data!.imageUrls![0]),
+                      ),
+                      Container(
+                        height: screenWidth * 0.2,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: snapshot.data!.imageUrls!.length,
+                          itemBuilder: (context, index) {
+                            // return Image.asset(snapshot.data!.imageUrls![index]);
+                            return Padding(
+                              padding: const EdgeInsets.only(left: 10, top: 5),
+                              child: Image.network(
+                                snapshot.data!.imageUrls![index],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.primary,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Center(
+                          child: DataTable(columns: <DataColumn>[
+                            DataColumn(label: Text('Information')),
+                            DataColumn(label: Text('value'))
+                          ], rows: <DataRow>[
+                            DataRow(cells: [
+                              DataCell(Text('location')),
+                              DataCell(Text(snapshot.data!.place!.name!))
+                            ]),
+                            DataRow(cells: [
+                              DataCell(Text('price')),
+                              DataCell(Text('${snapshot.data!.price!} Rwf'))
+                            ]),
+                            DataRow(cells: [
+                              DataCell(Text('area')),
+                              DataCell(Text('${snapshot.data!.size!} m'))
+                            ]),
+                            DataRow(cells: [
+                              DataCell(Text('bedrooms')),
+                              DataCell(Text('${snapshot.data!.bedrooms}'))
+                            ]),
+                            DataRow(cells: [
+                              DataCell(Text('bathrooms')),
+                              DataCell(Text('${snapshot.data!.bathrooms}'))
+                            ]),
+                            DataRow(cells: [
+                              DataCell(Text('parking')),
+                              DataCell(Icon(snapshot.data!.hasParking!
+                                  ? Icons.check_outlined
+                                  : Icons.clear))
+                            ]),
+                            DataRow(cells: [
+                              DataCell(Text('year built')),
+                              DataCell(Text(
+                                  '${snapshot.data!.yearBuilt!.split('-')[0]}'))
+                            ]),
+                            DataRow(cells: [
+                              DataCell(Text('category')),
+                              DataCell(
+                                  Text('${snapshot.data!.category!.name!}'))
+                            ]),
+                          ]),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        padding: EdgeInsets.all(13),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Theme.of(context).colorScheme.background),
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 15),
+                              child: Text(
+                                'Description',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                            HtmlWidget(
+                              ''' ${snapshot.data!.details!} ''',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
