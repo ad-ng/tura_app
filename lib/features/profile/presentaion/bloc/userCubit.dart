78238cca92f1c40949608c47c4793ac32b2963a9
ad-ng <adolphengoga@gmail.com>
2025-02-11 10:05:32 +0200

    adding user statemanagement

---
+++ lib/features/profile/presentaion/bloc/userCubit.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tura_app/features/login/data/models/user_model.dart';
+import 'package:tura_app/features/profile/domain/repository/user_repo.dart';
+
+class Usercubit extends Cubit<UserState>{
+  final UserRepo _userRepo;
+
+Usercubit(this._userRepo) : super (UserInitial());
+
+  Future fetchUser() async {
+    emit(UserLoading());
+
+    try {
+      final response = await _userRepo.fetchUser();
+      print('Fetched user: $response'); // Log the response
+      emit(UserSuccess(response));
+    } catch (e) {
+      print('Error fetching properties: $e'); // Log any error
+      emit(UserError(e.toString()));
+    }
+  }
+}
+
+abstract class UserState{}
+
+class UserInitial extends UserState {}
+
+class UserLoading extends UserState {}
+
+class UserSuccess extends UserState {
+  final UserModel response; // Response from the API
+  UserSuccess(this.response);
+}
+
+class UserError extends UserState {
+  final String message; // Error message
+  UserError(this.message);
+}
+
+
+
+
