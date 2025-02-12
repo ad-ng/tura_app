c73d95c10f32c2bed956be7bc95f08a0d5cf0395
ad-ng <adolphengoga@gmail.com>
2025-02-12 18:23:26 +0200

    adding shares sent cubit

---
+++ lib/features/shares/presentation/bloc/sharesSentCubit.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tura_app/features/shares/data/model/sharemodel.dart';
+import 'package:tura_app/features/shares/domain/repositories/share_repo.dart';
+
+class SharesSentCubit extends Cubit<SharesSentState> {
+  final ShareRepo _shareRepo;
+
+  SharesSentCubit(this._shareRepo) : super(SharesSentInitial()) {
+    fetchSharesSent();
+  }
+
+  Future fetchSharesSent() async {
+    emit(SharesSentLoading());
+
+    try {
+      final response = await _shareRepo.fetchSentShares();
+      print('Fetched user: $response'); // Log the response
+      emit(SharesSentSuccess(response));
+    } catch (e) {
+      print('Error fetching properties: $e'); // Log any error
+      emit(SharesSentError(e.toString()));
+    }
+  }
+}
+
+abstract class SharesSentState {}
+
+class SharesSentInitial extends SharesSentState {}
+
+class SharesSentLoading extends SharesSentState {}
+
+class SharesSentSuccess extends SharesSentState {
+  final List<Sharemodel> response; // Response from the API
+  SharesSentSuccess(this.response);
+}
+
+class SharesSentError extends SharesSentState {
+  final String message; // Error message
+  SharesSentError(this.message);
+}
