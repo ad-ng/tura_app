f72ea59079ef52258d6428c992b2f30733215610
ad-ng <adolphengoga@gmail.com>
2025-02-22 14:26:54 +0200

    adding favorites page

---
+++ lib/features/favorites/presentation/pages/favorites.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Favorites_page extends StatelessWidget {
+  const Favorites_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favorites'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
