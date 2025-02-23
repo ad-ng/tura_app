cc7a9a7f48423c8d49f066befa0e189804c430de
ad-ng <adolphengoga@gmail.com>
2025-02-23 09:08:27 +0200

    adding favorite card

---
+++ lib/features/favorites/presentation/widgets/favoriteCard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Favoritecard extends StatelessWidget {
+  const Favoritecard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
