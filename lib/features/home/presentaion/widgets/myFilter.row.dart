63099287079f54eb7731b7f348bddb3db8cd7e63
ad-ng <adolphengoga@gmail.com>
2025-03-02 08:15:27 +0200

    adding myfilter row component

---
+++ lib/features/home/presentaion/widgets/myFilter.row.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MyFilterRow extends StatefulWidget {
+  final String dropDownTitle;
+
+  final String firstDropDown;
+  final itemRanges;
+  final onChanged;
+
+  const MyFilterRow({
+    super.key,
+    required this.dropDownTitle,
+    required this.firstDropDown,
+    required this.itemRanges,
+    required this.onChanged,
+  });
+
+  @override
+  State<MyFilterRow> createState() => _MyFilterRowState();
+}
+
+class _MyFilterRowState extends State<MyFilterRow> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+        Text(
+          widget.dropDownTitle,
+          style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.surface),
+        ),
+        SizedBox(width: MediaQuery.of(context).size.width * 0.02),
+        DropdownButton<String>(
+            items: widget.itemRanges,
+            value: widget.firstDropDown, // Use the valid dropDownValue here
+            onChanged: widget.onChanged),
+      ],
+    );
+  }
+}
