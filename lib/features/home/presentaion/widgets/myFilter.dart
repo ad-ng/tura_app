8fd66eebacdd4b000940a2c8c27b882c0121f6f7
ad-ng <adolphengoga@gmail.com>
2025-03-02 07:54:27 +0200

    adding property prices

--- lib/features/home/presentaion/widgets/myFilter.dart
+++ lib/features/home/presentaion/widgets/myFilter.dart
@@ -17,12 +17,20 @@ class _MyFilterState extends State<MyFilter> {
       value: 'All Ranges',
     ),
     DropdownMenuItem(
-      child: Text('Under RWF 50,000,000'),
+      child: Text('Under RWF 50 M'),
       value: '[0,50]',
     ),
     DropdownMenuItem(
-      child: Text('RWF 50,000,000 - RWF 100,000,000'),
+      child: Text('RWF 50 M -  100 M'),
       value: 'two',
+    ),
+    DropdownMenuItem(
+      child: Text('RWF 100 M - 150 M'),
+      value: 'three',
+    ),
+    DropdownMenuItem(
+      child: Text('RWF 150 M - 200 M'),
+      value: 'four',
     )
   ];
 
@@ -35,10 +43,12 @@ class _MyFilterState extends State<MyFilter> {
         Mysearch(textEditingController: textEditingController),
         Row(
           children: [
-            Text('Property Prices'),
-            SizedBox(
-              width: 10,
+            SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+            Text(
+              'Property Prices:',
+              style: TextStyle(fontSize: 16),
             ),
+            SizedBox(width: MediaQuery.of(context).size.width * 0.02),
             DropdownButton<String>(
               items: priceRanges,
               value: dropDownValue, // Use the valid dropDownValue here
