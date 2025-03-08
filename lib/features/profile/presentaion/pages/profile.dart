9b9a382c1ccd1c8b0fb795e34635a1f815769786
ad-ng <adolphengoga@gmail.com>
2025-03-08 15:39:25 +0200

    updating profile

--- lib/features/profile/presentaion/pages/profile.dart
+++ lib/features/profile/presentaion/pages/profile.dart
@@ -28,35 +28,21 @@ class Profile extends StatelessWidget {
                 final user = state.response;
                 return Column(
                   children: [
-                    Stack(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.symmetric(
-                              horizontal: screenWidth * 0.01),
-                          height: screenHeight * 0.21,
-                          width: screenWidth,
+                    Container(
+                      margin: EdgeInsets.only(
+                        top: screenHeight * 0.03,
+                        bottom: screenHeight * 0.01,
+                      ),
+                      child: Center(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(90),
                           child: Image.network(
-                            user.coverImg!,
+                            user.profileImg!,
                             fit: BoxFit.cover,
+                            height: screenHeight * 0.15,
                           ),
                         ),
-                        Container(
-                          margin: EdgeInsets.only(
-                            top: screenHeight * 0.13,
-                            bottom: screenHeight * 0.01,
-                          ),
-                          child: Center(
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(90),
-                              child: Image.network(
-                                user.profileImg!,
-                                fit: BoxFit.cover,
-                                height: screenHeight * 0.15,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                     Center(
                       child: Text(
@@ -67,40 +53,165 @@ class Profile extends StatelessWidget {
                         ),
                       ),
                     ),
-                    Container(
-                      color: Theme.of(context).colorScheme.secondary,
-                      child: DataTable(
-                        dividerThickness: 0,
-                        columns: <DataColumn>[
-                          DataColumn(label: Text('Information')),
-                          DataColumn(label: Text('value'))
-                        ],
-                        rows: <DataRow>[
-                          DataRow(cells: [
-                            DataCell(Text('username')),
-                            DataCell(Text(user.username!))
-                          ]),
-                          DataRow(cells: [
-                            DataCell(Text('email')),
-                            DataCell(Text('${user.email}'))
-                          ]),
-                          DataRow(cells: [
-                            DataCell(Text('gender')),
-                            DataCell(Text('${user.gender}'))
-                          ]),
-                          DataRow(cells: [
-                            DataCell(Text('Phone Number')),
-                            DataCell(Text('${user.phoneNumber}'))
-                          ]),
-                          DataRow(cells: [
-                            DataCell(Text('address')),
-                            DataCell(Text('${user.address}'))
-                          ]),
-                          DataRow(cells: [
-                            DataCell(Text('born at')),
-                            DataCell(Text(
-                                '${user.dob!.split('-')[0]} - ${user.dob!.split('-')[1]} - ${user.dob!.split('-')[2].substring(0, 2)}'))
-                          ]),
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Joined:   ${user.createdAt.split('-')[0]} / ${user.createdAt.split('-')[1]} / ${user.createdAt.split('-')[2].substring(0, 2)}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.secondary),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      margin: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          ListTile(
+                            leading: Icon(Icons.person),
+                            title: Text(
+                              'username',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 20,
+                              ),
+                            ),
+                            trailing: Text(
+                              '@${user.username!}',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.secondary,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.verified),
+                            title: Text(
+                              'Verification',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 20,
+                              ),
+                            ),
+                            trailing: Text(
+                              '${user.isVerified}',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.secondary,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.email),
+                            title: Text(
+                              'Email',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 20,
+                              ),
+                            ),
+                            trailing: Text(
+                              user.email!,
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.secondary,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.phone),
+                            title: Text(
+                              'Phone',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 20,
+                              ),
+                            ),
+                            trailing: Text(
+                              user.phoneNumber!,
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.secondary,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.home_work_rounded),
+                            title: Text(
+                              'Address',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 20,
+                              ),
+                            ),
+                            trailing: Text(
+                              user.address!,
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.secondary,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.calendar_month_rounded),
+                            title: Text(
+                              'Born',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 20,
+                              ),
+                            ),
+                            trailing: Text(
+                              '${user.dob!.split('-')[0]} - ${user.dob!.split('-')[1]} - ${user.dob!.split('-')[2].substring(0, 2)}',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.secondary,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.male),
+                            title: Text(
+                              'Gender',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 20,
+                              ),
+                            ),
+                            trailing: Text(
+                              user.gender!,
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.secondary,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.settings),
+                            title: Text(
+                              'Role',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 20,
+                              ),
+                            ),
+                            trailing: Text(
+                              user.role!,
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.secondary,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                     )
