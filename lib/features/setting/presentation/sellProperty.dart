9ff48de0c17e42c91e0aceeecb0c3e883d06c218
ad-ng <adolphengoga@gmail.com>
2025-03-10 08:13:05 +0200

    referrencing sell your property page

---
+++ lib/features/setting/presentation/sellProperty.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Sellproperty extends StatelessWidget {
+  const Sellproperty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
